be9d8f00e7f1e259621f4b1d1cbe9b08d3928fbd
Bo <Zboyang@umich.edu>
2019-11-23 03:53:42 -0500

    Added combo pressing

--- Assets/Scripts/Player/Player_Controller.cs
+++ Assets/Scripts/Player/Player_Controller.cs
@@ -10,15 +10,22 @@ public class Player_Controller : MonoBehaviour
     public bool DEBUG = false;
     [Space(10)]
 
-    public float move_speed = 100f;
+    public float move_speed = 20f;
+    public float combo_wait_time_sec = 0.15f;
 
     /* PRIVATE */
 
     private Rigidbody self_rbody;
     private string combo_string = "";
     private int move = 0;
+    private float last_key_pressed_time = 0;
 
     /* USER FUNCTIONS */
+
+    private void ComboKeyPressed()
+    {
+        last_key_pressed_time = Time.time;
+    }
 
     private void PollInput()
     {
@@ -43,63 +50,107 @@ public class Player_Controller : MonoBehaviour
         // Get combo input
         if (Input.GetButtonDown("Forward"))
         {
+            ComboKeyPressed();
             combo_string += ">";
         }
         else if (Input.GetButtonDown("Back"))
         {
+            ComboKeyPressed();
             combo_string += "<";
         }
         else if (Input.GetButtonDown("Jump"))
         {
+            ComboKeyPressed();
             combo_string += "^";
         }
         else if (Input.GetButtonDown("Crouch"))
         {
+            ComboKeyPressed();
             combo_string += "v";
         }
         else if (Input.GetButtonDown("Block"))
         {
+            ComboKeyPressed();
             combo_string += "b";
         }
         else if (Input.GetButtonDown("Punch"))
         {
+            ComboKeyPressed();
             combo_string += "p";
         }
         else if (Input.GetButtonDown("Kick"))
         {
+            ComboKeyPressed();
             combo_string += "k";
         }
     }
 
     private void Movement()
     {
-        self_rbody.AddForce(new Vector3(move_speed * move, 0));
+        self_rbody.AddForce(new Vector3(move_speed * move * Time.fixedDeltaTime * 100, 0));
+    }
+
+    // Returns if "combo_string" is a valid combo
+    private bool ValidCombo()
+    {
+        // A combo is only valid if it exists and
+        // the player has access to it
+
+        return false;
+    }
+
+    private void ExecuteCombo()
+    {
+        // Check combo_string:
+        // If valid combo, execute the combo
+        if (ValidCombo())
+        {
+
+        }
+        // If invalid combo, check the last key,
+        // if the last key is a non-lateral movement key, execute it
+        else
+        {
+
+        }
+    }
+
+    private void ProcessCombos()
+    {
+        // Get time elapsed since the last time a combo button was pressed
+        float combo_elapsed_time = Time.time - last_key_pressed_time;
+        
+        // If the time elapsed is greater than the set amount, then execute the combo
+        if (combo_elapsed_time > combo_wait_time_sec && combo_string != "")
+        {
+            if (DEBUG)
+            {
+                Debug.Log("Combo");
+                Debug.Log(combo_string);
+            }
+            ExecuteCombo();
+            combo_string = ""; // Reset the combo string
+        }
     }
 
     /* UNITY FUNCTIONS */
 
-    // Start is called before the first frame update
     private void Awake()
     {
         self_rbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        if (DEBUG)
-        {
-            Debug.Log("Combo string");
-            Debug.Log(combo_string);
-
-            Debug.Log("Move");
-            Debug.Log(move);
-        }
-
         // Get inputs
         PollInput();
 
-        // Move character
+        ProcessCombos();
+    }
+
+    private void FixedUpdate()
+    {
+        // Move character, in fixed update to smooth movement
         Movement();
     }
 }
