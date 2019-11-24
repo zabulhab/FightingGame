ec473e08f2ccc2211bb3dea932139a14615eb1b8
zabulhab <zabulhab@colby.edu>
2019-11-24 03:24:06 -0500

    Added keeping track of which specials should stay disabled between rounds

--- Assets/Scripts/AbilitySelectScreenController.cs
+++ Assets/Scripts/AbilitySelectScreenController.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using TMPro; 
+using TMPro;
+using System;
 
 // TODO: Remember the existing selected moves from last round(s)
 // and only enable choosing a move for the last, open slot
 public class AbilitySelectScreenController : MonoBehaviour
 {
     // TODO: Store a list of all abilities
-
+    public List<GameObject> AllAbilityButtons;
 
     public TextMeshProUGUI selectionHeader;
     //TODO: maintain arrays with the players abilities at all times.
@@ -23,11 +24,11 @@ public class AbilitySelectScreenController : MonoBehaviour
     public int maxCapacity = 4; 
 
     //Capacity starts at 1 for each player
-    public int player1Capacity = 1;
-    public int player2Capacity = 1;
+    public int player1CurCapacity = 1;
+    public int player2CurCapacity = 1;
 
-    // Negative one if game is just starting.
-    // 1 if player 1. 2 if for player 2. Must be determined upon loading the scene. 
+    // 0 if pre-round 1 (start of bout)
+    // 1 for player 1. 2 for player 2. Must be determined upon loading the scene. 
     private int previousWinner = 0; // ***
 
     // The last move we tried to select. 
@@ -42,12 +43,34 @@ public class AbilitySelectScreenController : MonoBehaviour
         previousWinner = GameManager.GetLastWinner();
 
         // the capacities should be the same as the round numbers
-        player1Capacity = GameManager.GetCurRoundNum();
-        player2Capacity = GameManager.GetCurRoundNum();
+        player1CurCapacity = GameManager.GetCurRoundNum();
+        player2CurCapacity = GameManager.GetCurRoundNum();
 
-        // TODO: Enable moves in moves list based on all abilties saved to gamemanager
-        // for now:
-        LoadSelectedSlots();
+        InitializeAbilitySlotSelectability();
+        InitializePlayerSelectionSlotsForRound();
+    }
+
+    /// <summary>
+    /// Initializes the selectability of each special move slot
+    /// </summary>
+    private void InitializeAbilitySlotSelectability()
+    {
+        List<int> disabledMoves = GameManager.GetAllDisabledSpecialIDs();
+
+        if (disabledMoves == null)
+        {
+            Debug.Log("DISABLED MOVES EMPTY");
+            return;
+        }
+
+        // get the list of ints corresponding to buttons that should be disabled
+        foreach (GameObject abilityButton in AllAbilityButtons)
+        {
+            if (disabledMoves.Contains(abilityButton.GetComponent<AbilityButtonUIControl>().MovesScriptMoveID))
+            {
+                abilityButton.GetComponent<AbilityButtonUIControl>().DisableButton();
+            }
+        }
     }
 
     //On update, update the header depending on who should be selecting their abilities now. 
@@ -81,9 +104,9 @@ public class AbilitySelectScreenController : MonoBehaviour
     }
 
     /// <summary>
-    /// Give both players an additional move slot on scene load 
+    /// Give both players an additional move slot on scene load.
     /// </summary>
-    public void LoadSelectedSlots()
+    public void InitializePlayerSelectionSlotsForRound()
     {
         //if(previousWinner == 1 && player2Capacity < maxCapacity)
         //{
@@ -100,22 +123,21 @@ public class AbilitySelectScreenController : MonoBehaviour
         // TODO: Account for max capacity? Probably fine tho...
         for (int i = 0;  i < player1SlotButtons.Length; i++)
         {
-            if (i < player1Capacity)
+            if (i < player1CurCapacity)
             {
                 player1SlotButtons[i].SetActive(true);
             }
         }
         for (int i = 0; i < player2SlotButtons.Length; i++)
         {
-            if (i < player2Capacity)
+            if (i < player2CurCapacity)
             {
                 player2SlotButtons[i].SetActive(true);
             }
         }
 
-        player1SlotButtons[player1Capacity-1].SetActive(true);
-        player2SlotButtons[player2Capacity-1].SetActive(true);
-
+        player1SlotButtons[player1CurCapacity-1].SetActive(true);
+        player2SlotButtons[player2CurCapacity-1].SetActive(true);
     }
 
     /// <summary>
@@ -132,10 +154,14 @@ public class AbilitySelectScreenController : MonoBehaviour
             // OR if we won and the losing player has confirmed
             if (previousWinner != 1 || previousWinner == 1 && player2Confirmed)
             {
-                TextMeshProUGUI[] player1Move = player1SlotButtons[player1Capacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
+                TextMeshProUGUI[] player1Move = player1SlotButtons[player1CurCapacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
                 player1Slots.Add(int.Parse(player1Move[0].text));
                 player1Confirmed = true;
                 DisableLastSelectedMoveButton();
+
+                // Add to P1 moves and remove from selectable moves
+                GameManager.AddToP1SpecialMoveInts(LastPressedMoveButton.GetComponent<AbilityButtonUIControl>().MovesScriptMoveID);
+                GameManager.AddToDisabledSpecialIDs(LastPressedMoveButton.GetComponent<AbilityButtonUIControl>().MovesScriptMoveID);
             }
         }
 
@@ -145,10 +171,14 @@ public class AbilitySelectScreenController : MonoBehaviour
             // OR if we won and the losing player has confirmed
             if (previousWinner != 2 || previousWinner == 2 && player1Confirmed)
             {
-                TextMeshProUGUI[] player2Move = player2SlotButtons[player2Capacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
+                TextMeshProUGUI[] player2Move = player2SlotButtons[player2CurCapacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
                 player2Slots.Add(int.Parse(player2Move[0].text));
                 player2Confirmed = true;
                 DisableLastSelectedMoveButton();
+
+                // Add to P2 moves and remove from selectable moves
+                GameManager.AddToP2SpecialMoveInts(LastPressedMoveButton.GetComponent<AbilityButtonUIControl>().MovesScriptMoveID);
+                GameManager.AddToDisabledSpecialIDs(LastPressedMoveButton.GetComponent<AbilityButtonUIControl>().MovesScriptMoveID);
             }
         }
 
@@ -169,8 +199,8 @@ public class AbilitySelectScreenController : MonoBehaviour
         {
             // TODO: Account for random starting player
             // if p1 hasn't confirmed, map it to p1, else map it to p2
-            TextMeshProUGUI[] prevLoserOpenSlotText = !player1Confirmed ? player1SlotButtons[player1Capacity-1].GetComponentsInChildren<TextMeshProUGUI>() :
-                                                                   player2SlotButtons[player2Capacity-1].GetComponentsInChildren<TextMeshProUGUI>();
+            TextMeshProUGUI[] prevLoserOpenSlotText = !player1Confirmed ? player1SlotButtons[player1CurCapacity-1].GetComponentsInChildren<TextMeshProUGUI>() :
+                                                                   player2SlotButtons[player2CurCapacity-1].GetComponentsInChildren<TextMeshProUGUI>();
             prevLoserOpenSlotText[0].text = currentButton[0].text;
         }
         else
@@ -178,25 +208,25 @@ public class AbilitySelectScreenController : MonoBehaviour
             // if p2 lost and hasn't confirmed yet, give to p2 slot
             if (previousWinner == 1 && !player2Confirmed)
             {
-                TextMeshProUGUI[] prevLoserOpenSlotText = player2SlotButtons[player2Capacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
+                TextMeshProUGUI[] prevLoserOpenSlotText = player2SlotButtons[player2CurCapacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
                 prevLoserOpenSlotText[0].text = currentButton[0].text;
             }
             // if p2 lost but has confirmed, give to p1 slot
             else if (previousWinner == 1 && player2Confirmed && !player1Confirmed)
             {
-                TextMeshProUGUI[] prevLoserOpenSlotText = player1SlotButtons[player1Capacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
+                TextMeshProUGUI[] prevLoserOpenSlotText = player1SlotButtons[player1CurCapacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
                 prevLoserOpenSlotText[0].text = currentButton[0].text;
             }
             // if p1 lost and hasn't confirmed, give to p2 slot
             else if (previousWinner == 2 && !player1Confirmed)
             {
-                TextMeshProUGUI[] prevLoserOpenSlotText = player1SlotButtons[player1Capacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
+                TextMeshProUGUI[] prevLoserOpenSlotText = player1SlotButtons[player1CurCapacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
                 prevLoserOpenSlotText[0].text = currentButton[0].text;
             }
             // if p1 lost but has confirmed, give to p2 slot
             else if (previousWinner == 2 && player1Confirmed && !player2Confirmed)
             {
-                TextMeshProUGUI[] prevLoserOpenSlotText = player2SlotButtons[player2Capacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
+                TextMeshProUGUI[] prevLoserOpenSlotText = player2SlotButtons[player2CurCapacity - 1].GetComponentsInChildren<TextMeshProUGUI>();
                 prevLoserOpenSlotText[0].text = currentButton[0].text;
             }
 
@@ -220,15 +250,6 @@ public class AbilitySelectScreenController : MonoBehaviour
     public void EnableMoveButton()
     {
         LastPressedMoveButton.GetComponent<AbilityButtonUIControl>().EnableButton();
-    }
-
-    /// <summary>
-    /// Just in case; was for canceling selection
-    /// </summary>
-    public void resetSelection()
-    {
-        player1Confirmed = false;
-        player2Confirmed = false;
     }
 
     public void StartGame()
