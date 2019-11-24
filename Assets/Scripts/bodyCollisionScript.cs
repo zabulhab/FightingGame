697e91d5b0b7902848714bf82d6f3b25ebf237a8
Nigel Charleston <ndcharle@umich.edu>
2019-11-23 21:05:08 -0500

    Began restructuring the colliders on player for collision script logic.

---
+++ Assets/Scripts/bodyCollisionScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bodyCollisionScript : MonoBehaviour
+{
+    public GameObject thisBodyPartsTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //If the opposing player hits the current players body part
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string tagOfBodyPartCollidedWith = collision.transform.tag;
+        string tagOfThisPartsTransform = thisBodyPartsTransform.tag;
+
+        if (tagOfBodyPartCollidedWith == "WeakPoint" && tagOfThisPartsTransform == "AttackPoint")
+        {
+
+        }
+        else if(tagOfBodyPartCollidedWith == "AttackPoint" && tagOfThisPartsTransform == "WeakPoint")
+        {
+
+        }
+        
+    }
+}
