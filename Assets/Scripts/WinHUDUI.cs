08d7cff7e8e6902358ee8571f657d560f7483bdc
zabulhab <zabulhab@colby.edu>
2019-11-24 15:47:43 -0500

    Added a UI script but it isn't working, ignore

---
+++ Assets/Scripts/WinHUDUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WinHUDUI : MonoBehaviour
+{
+    public GameObject winGameObject;
+
+    // Start is called before the first frame update
+    public void DisplayWinScreen()
+    {
+        //StartCoroutine(Win());
+
+    }
+
+    private IEnumerator Win()
+    {
+        winGameObject.SetActive(true);
+        winGameObject.GetComponent<TextMeshProUGUI>().text = "WINNER: P" + GameManager.GetLastWinner();
+        yield return new WaitForSeconds(2);
+    }
+
+}
