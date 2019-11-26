a8b8d547f3f33899da266d6cf0f7d30fdbb267e5
Treknishion <treknishion12@yahoo.com>
2019-11-26 02:07:14 -0500

    Started Implementing Art Assets

---
+++ Assets/Art/BallSpin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().angularVelocity = new Vector3(Random.Range(1, 5), Random.Range(1, 5), Random.Range(1, 5));
+    }
+}
---
+++ Assets/Art/ProjectileMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public int master;
+    public Vector3 spawnPoint;
+    private int speedMult;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = spawnPoint;
+        if (master == 1)
+        {
+            speedMult = 1;
+            this.tag = "P1_AP";
+            this.GetComponent<bodyCollisionScript>().enemyTagPart = "P2_WP";
+        }
+        else
+        {
+            speedMult = -1;
+            this.tag = "P2_AP";
+            this.GetComponent<bodyCollisionScript>().enemyTagPart = "P1_WP";
+        }
+        this.GetComponent<Rigidbody>().velocity = new Vector3(5f * speedMult, 0f, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this);
+    }
+}
